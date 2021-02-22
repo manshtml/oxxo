94b32a40e10bd67ab76ff8677b97b85acb7bea68
manshtml <76622958+manshtml@users.noreply.github.com>
2021-02-22 15:34:05 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	let i;
+	let slides = document.getElementsByClassName("introSlider")
+
+	if (n >slides.length) {
+		slideIndex = 1
+	}
+	if (n < 1) {
+		slideIndex=slide.length
+	}
+	for (i=0; i <slides.length ; i++) {
+		slides[i].style.display= "none";
+	}
+	slides[slideIndex-1].style.display = "block";
+}
